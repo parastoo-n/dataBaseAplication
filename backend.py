051dd18a1b719c3b49735a73846a3676602e10e1
parastoo naderi <roniaania@gmail.com>
2024-11-03 23:17:41 +0330

    database created and The delete function has been defined

--- backend.py
+++ backend.py
@@ -3,14 +3,14 @@ import sqlite3
 def connect():
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
-    cur.execute("CREAT TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY,title text,author text, year INTEGER,isbn INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY,title text,author text, year INTEGER,isbn INTEGER)")
     conn.commit()
     conn.close()
 
 def insert(title,author,year,isnb):
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO book (NULL ,?,?,?,?)",(title, author,year,isnb))
+    cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title, author,year,isnb))
     conn.commit()
     conn.close()
 
@@ -23,8 +23,19 @@ def view():
     conn.close()
     return rows
 
+def search(title="",author="",year="",isnb=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isnb))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
 
 
 connect()
-insert("")
+# insert("python ebook","mohammad",2015,2565)
+insert("csharp","sara",2015,2565)
+# print(view())
+print(search(author="sara"))
    
